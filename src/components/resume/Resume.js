59f74ef608e331b50963d72ea05b16b7f5a9df2f
Benjamin Mehrdad <75631604+Benjamin-GitHub@users.noreply.github.com>
2023-10-21 22:43:19 +0100

    temporary changing of resume button with a modal

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Link } from '@material-ui/core';
 import { TextDecrypt } from '../content/TextDecrypt';
-import ResumePDF from './../../assets/CV Benjamin Mehrdad.pdf';
-import {
-  ResumeIcon
-} from '../content/ResumeButton';
+// import ResumePDF from './../../assets/CV Benjamin Mehrdad.pdf';
+import {ResumeIcon} from '../content/ResumeButton';
+
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
 const useStyles = makeStyles((theme) => ({
+  modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
   footerText: {
     position: 'fixed',
     bottom: theme.spacing(6),
@@ -23,22 +30,88 @@ const useStyles = makeStyles((theme) => ({
   
 }));
 
+// const useStyles = makeStyles((theme) => ({
+//   modal: {
+//     display: 'flex',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   paper: {
+//     backgroundColor: theme.palette.background.paper,
+//     border: '2px solid #000',
+//     boxShadow: theme.shadows[5],
+//     padding: theme.spacing(2, 4, 3),
+//   },
+// }));
+
+
+// export const Resume = () => {
+//   const classes = useStyles();
+
+//   return (
+//     <Link
+//       color='inherit'
+//       underline='none'
+//       href= {`${ResumePDF}`}
+//       target='_blank'
+//       rel='noopener noreferrer'
+//       className={classes.footerText}
+//     >
+//       <ResumeIcon />
+//       <Typography component='span'>
+//         <TextDecrypt text={' Resume'} />
+//       </Typography>
+//     </Link>
+//   );
+// };
+
 export const Resume = () => {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <Link
+    <div>
+      <Link
       color='inherit'
       underline='none'
-      href= {`${ResumePDF}`}
+      onClick={handleOpen}
+      // href= {`${ResumePDF}`}
       target='_blank'
       rel='noopener noreferrer'
       className={classes.footerText}
     >
-      <ResumeIcon />
-      <Typography component='span'>
-        <TextDecrypt text={' Resume'} />
-      </Typography>
-    </Link>
+       <ResumeIcon />
+       <Typography component='span'>
+         <TextDecrypt text={' Resume'} />
+       </Typography>
+     </Link>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">Please contact via Email</h2>
+            <p id="transition-modal-description">benjaminmhrdd@gmail.com</p>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
   );
-};
+}
